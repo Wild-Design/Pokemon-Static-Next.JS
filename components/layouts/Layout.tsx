0b6a257509b97194255710ecb4422d9939cb55c3
Wild-Design <brenneke_ruger@hotmail.com>
2023-05-25 21:27:20 -0300

    App terminada lista para el depliegue :D

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import { FC, ReactNode } from 'react';
 import { NavBar } from '../ui';
 
+const origin = typeof window === 'undefined' ? '' : window.location.origin;
+
 export const Layout: FC<{ children: ReactNode; title: string }> = ({
   children,
   title,
@@ -16,6 +18,13 @@ export const Layout: FC<{ children: ReactNode; title: string }> = ({
           content={`Información sobre el pokemon ${title}`}
         />
         <meta name='keywords' content={`${title}, pokemon, pokedex`} />
+
+        <meta property='og:title' content={`Información sobre ${title}`} />
+        <meta
+          property='og:description'
+          content={`Esta es la página sobre ${title}`}
+        />
+        <meta property='og:image' content={`${origin}/img/banner.png`} />
       </Head>
       <NavBar />
       <main>{children}</main>
