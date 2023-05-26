eff28d0802b402658b79bbcc8f6715d9c16327e3
Wild-Design <brenneke_ruger@hotmail.com>
2023-05-25 21:38:25 -0300

    Arreglo imagen

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -24,7 +24,7 @@ export const Layout: FC<{ children: ReactNode; title: string }> = ({
           property='og:description'
           content={`Esta es la página sobre ${title}`}
         />
-        <meta property='og:image' content={`${origin}/img/banner.png`} />
+        <meta property='og:image' content={`${origin}/img/Gengar-jpg`} />
       </Head>
       <NavBar />
       <main>{children}</main>
